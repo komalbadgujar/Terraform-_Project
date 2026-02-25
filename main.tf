2c0f6d84f58229c574cd7c2537942816fbed144f
komalbadgujar <komalbadgujar1057@gmail.com>
2026-02-26 02:45:56 +0530

    modified

--- main.tf
+++ main.tf
@@ -1,27 +1,22 @@
-provider "aws" {
-    region = "ap-south-1"
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
 }
 
-resource "aws_vpc" "my-vpc" {
-    cidr_block = "10.0.0.0/16"
-    enable_dns_support = true
+resource "aws_subnet" "public1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
+  map_public_ip_on_launch = true
 }
 
-resource "aws-subnet" "public1" {
-    vpc_id =aws_vpc_main.vpc_id
-    cidr_block = "10.0.1.0/24"
-    availability_zone = "ap-south-1a"
-    map_public_ip_on_launch = true
-}
-
-resource "aws-subnet" "public2" {
-    vpc_id =aws_vpc_main.vpc_id
-    cidr_block = "10.0.2.0/24"
-    availability_zone = "ap-south-1b"
-    map_public_ip_on_launch = true
+resource "aws_subnet" "public2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "ap-south-1b"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_internet_gateway" "igw" {
-    vpc_id = aws_vpc_main.id
-  
+  vpc_id = aws_vpc.main.id
 }
