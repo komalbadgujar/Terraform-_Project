003cdfa5e7cf63771ea0991a574d99ce1ebcc132
komalbadgujar <komalbadgujar1057@gmail.com>
2026-02-26 02:54:08 +0530

    modify

--- main.tf
+++ main.tf
@@ -3,14 +3,14 @@ resource "aws_vpc" "main" {
   enable_dns_hostnames = true
 }
 
-resource "aws_subnet" "public0" {
+resource "aws_subnet" "p-1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = true
 }
 
-resource "aws_subnet" "public2" {
+resource "aws_subnet" "p-2" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "ap-south-1b"
