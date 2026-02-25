f4b2676ba1b68919c48a47fdb3345a3665cfe04f
komalbadgujar <komalbadgujar1057@gmail.com>
2026-02-26 03:20:03 +0530

    changes

--- autoscaling.tf
+++ autoscaling.tf
@@ -3,12 +3,15 @@ resource "aws_launch_template" "web_template" {
     image_id = "ami-051a31ab2f4d498f5"
     instance_type = var.instance_type
 
-    user_data = <<EOF
-    #!/bin/bash
-    yum install -y httpd
-    systemctl start httpd
-    echo "<h1> hello from $(hostname)" > /var/www/html/index.html
-    EOF
+    user_data = base64encode(<<-EOF
+#!/bin/bash
+yum update -y
+yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
+echo "Hello from $(hostname)" > /var/www/html/index.html
+EOF
+  )
 
     
    network_interfaces {
